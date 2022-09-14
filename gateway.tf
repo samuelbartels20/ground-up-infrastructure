0c80c5cbf5cb451336ae5c98ab5f262632c05b86
Samuel Bartels <bartelssamuel20@gmail.com>
2022-09-14 23:41:31 +0000

    Update gateway.tf

--- gateway.tf
+++ gateway.tf
@@ -56,3 +56,55 @@ resource "aws_internet_gateway" "internet_gateway_for_VPC_C" {
     }
 
 }
+
+resource "aws_ec2_transit_gateway" "transit_gateway" {
+  description                     = "Transit Gateway with 3 VPCs, 2 subnets each"
+  default_route_table_association = "disable"
+  default_route_table_propagation = "disable"
+  tags                            = {
+    Name                          = "${var.scenario}"
+    scenario                      = "${var.scenario}"
+  }
+}
+
+# VPC attachment
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "transit_gateway_attachment_for_vpc_A" {
+  subnet_ids         = ["${aws_subnet.vpc_A_subnet_AZ1.id}", "${aws_subnet.vpc_A_subnet_AZ2.id}"]
+  transit_gateway_id = "${aws_ec2_transit_gateway.transit_gateway.id}"
+  vpc_id             = "${aws_vpc.vpc_A_cidr.id}"
+  transit_gateway_default_route_table_association = false
+  transit_gateway_default_route_table_propagation = false
+  tags               = {
+    Name             = "transit_gateway_attachment_for_vpc_A"
+    scenario         = "${var.scenario}"
+  }
+  depends_on = [aws_ec2_transit_gateway.transit_gateway]
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "transit_gateway_attachment_for_vpc_B" {
+  subnet_ids         = ["${aws_subnet.vpc_B_subnet_AZ1.id}", "${aws_subnet.vpc_B_subnet_AZ2.id}"]
+  transit_gateway_id = "${aws_ec2_transit_gateway.transit_gateway.id}"
+  vpc_id             = "${aws_vpc.vpc_B_cidr.id}"
+  transit_gateway_default_route_table_association = false
+  transit_gateway_default_route_table_propagation = false
+  tags               = {
+    Name             = "transit_gateway_attachment_for_vpc_B"
+    scenario         = "${var.scenario}"
+  }
+  depends_on = [aws_ec2_transit_gateway.transit_gateway]
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "transit_gateway_attachment_for_vpc_C" {
+  subnet_ids         = ["${aws_subnet.vpc_C_subnet_AZ1.id}", "${aws_subnet.vpc_C_subnet_AZ2.id}"]
+  transit_gateway_id = "${aws_ec2_transit_gateway.transit_gateway.id}"
+  vpc_id             = "${aws_vpc.vpc_C_cidr.id}"
+  transit_gateway_default_route_table_association = false
+  transit_gateway_default_route_table_propagation = false
+  tags               = {
+    Name             = "transit_gateway_attachment_for_vpc_C"
+    scenario         = "${var.scenario}"
+  }
+  depends_on = [aws_ec2_transit_gateway.transit_gateway]
+}
+
