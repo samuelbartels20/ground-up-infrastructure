5eac03fec4521e3c29cc6eb3c25e6e698fef4fbb
Samuel Bartels <bartelssamuel20@gmail.com>
2022-09-14 23:43:34 +0000

    Update routetables.tf

--- routetables.tf
+++ routetables.tf
@@ -58,7 +58,6 @@ resource "aws_route_table" "rt_for_VPC_C" {
 
 }
 
- 
 
 # #Associate Public Route Table to Public Subnets
 
@@ -110,9 +109,72 @@ resource "aws_route_table_association" "rt_association_for_VPC_C_subnet_AZ2" {
 
 }
 
+resource "aws_route" "VPC_A_rt_to_VPC_B_rt" {
+  route_table_id            = aws_vpc.vpc_A_cidr.main_route_table_id
+  destination_cidr_block    = aws_vpc.vpc_B_cidr.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.vpc_A_to_VPC_B.id
+}
 
+resource "aws_route" "VPC_B_rt_to_VPC_A_rt" {
+  route_table_id            = aws_vpc.vpc_B_cidr.main_route_table_id
+  destination_cidr_block    = aws_vpc.vpc_A_cidr.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.vpc_A_to_VPC_B.id
+}
 
+resource "aws_route" "VPC_A_rt_to_VPC_C_rt" {
+  route_table_id            = aws_vpc.vpc_A_cidr.main_route_table_id
+  destination_cidr_block    = aws_vpc.vpc_C_cidr.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.vpc_A_to_VPC_C.id
+}
+
+resource "aws_route" "VPC_C_rt_to_VPC_A_rt" {
+  route_table_id            = aws_vpc.vpc_C_cidr.main_route_table_id
+  destination_cidr_block    = aws_vpc.vpc_A_cidr.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.vpc_A_to_VPC_C.id
+}
  
+resource "aws_ec2_transit_gateway_route_table" "transit_gateway-rt" {
+  transit_gateway_id = "${aws_ec2_transit_gateway.transit_gateway.id}"
+  tags               = {
+    Name             = "transit_gateway-rt"
+    scenario         = "${var.scenario}"
+  }
+  depends_on = [aws_ec2_transit_gateway.transit_gateway]
+}
+
+
+resource "aws_ec2_transit_gateway_route_table_association" "tgw-rt-vpc-A-assoc" {
+  transit_gateway_attachment_id  = "${aws_ec2_transit_gateway_vpc_attachment.transit_gateway_attachment_for_vpc_A.id}"
+  transit_gateway_route_table_id = "${aws_ec2_transit_gateway_route_table.transit_gateway-rt.id}"
+}
+
+resource "aws_ec2_transit_gateway_route_table_association" "tgw-rt-vpc-B-assoc" {
+  transit_gateway_attachment_id  = "${aws_ec2_transit_gateway_vpc_attachment.transit_gateway_attachment_for_vpc_B.id}"
+  transit_gateway_route_table_id = "${aws_ec2_transit_gateway_route_table.transit_gateway-rt.id}"
+}
+
+resource "aws_ec2_transit_gateway_route_table_association" "tgw-rt-vpc-C-assoc" {
+  transit_gateway_attachment_id  = "${aws_ec2_transit_gateway_vpc_attachment.transit_gateway_attachment_for_vpc_C.id}"
+  transit_gateway_route_table_id = "${aws_ec2_transit_gateway_route_table.transit_gateway-rt.id}"
+}
+
+
+# Route Tables Propagations
+## This section defines which VPCs will be routed from each Route Table created in the Transit Gateway
+
+resource "aws_ec2_transit_gateway_route_table_propagation" "tgw-rt-to-vpc-A" {
+  transit_gateway_attachment_id  = "${aws_ec2_transit_gateway_vpc_attachment.transit_gateway_attachment_for_vpc_A.id}"
+  transit_gateway_route_table_id = "${aws_ec2_transit_gateway_route_table.transit_gateway-rt.id}"
+}
+
+resource "aws_ec2_transit_gateway_route_table_propagation" "tgw-rt-to-vpc-B" {
+  transit_gateway_attachment_id  = "${aws_ec2_transit_gateway_vpc_attachment.transit_gateway_attachment_for_vpc_B.id}"
+  transit_gateway_route_table_id = "${aws_ec2_transit_gateway_route_table.transit_gateway-rt.id}"
+}
+resource "aws_ec2_transit_gateway_route_table_propagation" "tgw-rt-to-vpc-C" {
+  transit_gateway_attachment_id  = "${aws_ec2_transit_gateway_vpc_attachment.transit_gateway_attachment_for_vpc_C.id}"
+  transit_gateway_route_table_id = "${aws_ec2_transit_gateway_route_table.transit_gateway-rt.id}"
+}
 
 
  
