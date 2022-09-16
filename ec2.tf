4d1a79012ac1bf4430b9599b6151b11366b574c9
Samuel Bartels <bartelssamuel20@gmail.com>
2022-09-16 01:52:36 +0000

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,4 @@
 
-
 resource "aws_instance" "app-server_for_VPC_A_subnet_AZ1" {
   instance_type          = var.instance_class
   ami                    = var.ami
@@ -51,4 +50,39 @@ resource "aws_instance" "app-server_for_VPC_C_subnet_AZ1" {
 }
 locals {
   vpc_c_account_id = aws_vpc.vpc_C_cidr.owner_id
+}
+
+
+resource "aws_instance" "app-server_for_OnPremVPC_private_subnet" {
+  instance_type          = var.instance_class
+  ami                    = var.ami
+  vpc_security_group_ids = [aws_security_group.security_group_for_OnPremVPC_private_subnet.id]
+  subnet_id              = aws_subnet.OnPremVpc_private_subnet.id
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
+  key_name               = var.instance_key
+
+  associate_public_ip_address = false
+  tags = {
+    Name = "app-server_for_OnPremVPC_private_subnet"
+  }
+}
+locals {
+  app-server_for_OnPremVPC_private_subnet = aws_vpc.OnPremVpc.owner_id
+}
+
+resource "aws_instance" "app-server_for_OnPremVPC_public_subnet" {
+  instance_type          = var.instance_class
+  ami                    = var.ami
+  vpc_security_group_ids = [aws_security_group.security_group_for_OnPremVPC_public_subnet.id]
+  subnet_id              = aws_subnet.OnPremVpc_public_subnet.id
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
+  key_name               = var.instance_key
+
+  associate_public_ip_address = false
+  tags = {
+    Name = "app-server_for_OnPremVPC_public_subnet"
+  }
+}
+locals {
+  app-server_for_OnPremVPC_public_subnet = aws_vpc.OnPremVpc.owner_id
 }
