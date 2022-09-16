37c872720a0d231181a3e757244b5acdfdf4babf
Samuel Bartels <bartelssamuel20@gmail.com>
2022-09-16 01:54:08 +0000

    Update security_groups.tf

--- security_groups.tf
+++ security_groups.tf
@@ -69,3 +69,51 @@ resource "aws_security_group" "security_group_for_VPC_C_instance" {
     "Name" = "EC2_instance_for_VPC_C_subnet_AZ1"
   }
 }
+
+
+resource "aws_security_group" "security_group_for_OnPremVPC_private_subnet" {
+  name        = "allow_web"
+  description = "allow inbound traffic"
+  vpc_id      = aws_vpc.OnPremVpc.id
+
+  ingress {
+    from_port   = "0"
+    to_port     = "65535"
+    protocol    = "all"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+     ipv6_cidr_blocks = ["::/0"]
+    from_port   = "0"
+    protocol    = "-1"
+    to_port     = "0"
+  }
+  tags = {
+    "Name" = "security_group_for_OnPremVPC_private_subnet"
+  }
+}
+
+
+resource "aws_security_group" "security_group_for_OnPremVPC_public_subnet" {
+  name        = "allow_web_access"
+  description = "allow inbound traffic"
+  vpc_id      = aws_vpc.OnPremVpc.id
+
+  ingress {
+    from_port   = "0"
+    to_port     = "65535"
+    protocol    = "all"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+     ipv6_cidr_blocks = ["::/0"]
+    from_port   = "0"
+    protocol    = "-1"
+    to_port     = "0"
+  }
+  tags = {
+    "Name" = "security_group_for_OnPremVPC_public_subnet"
+  }
+}
